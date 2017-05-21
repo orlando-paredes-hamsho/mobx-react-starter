3a47b0f7c23e9b6159bcd564242cdc880332e165
Orlando Paredes <orlando@turning.io>
2017-05-21 14:14:48 -0500

    patch: enforce mobx strict mode

--- src/models/userStore.js
+++ src/models/userStore.js
@@ -1,5 +1,7 @@
-import { observable, action } from 'mobx';
+import mobx, { observable, action } from 'mobx';
 import axios from 'axios';
+
+mobx.useStrict(true);
 
 class UserStore {
 	//Observable values can be watched by Observers
