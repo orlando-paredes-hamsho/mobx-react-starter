a75e78a027c0a559ee8c3ae0b4f4309d2deb24f8
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2019-07-23 14:39:33 -0500

    patch remove unnecessary tests

--- test/models/model.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import model from '../../src/models/model';
-
-describe('Model' , () => {
-	it('has something', () => {
-		expect(model.something).to.not.be.undefined;
-	});
-});
