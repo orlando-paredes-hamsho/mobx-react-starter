b69e0a87dc9ec492f33326670042ca85d03d6c2a
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2019-07-23 14:27:49 -0500

    patch: making it work again

--- test/components/app.test.js
+++ test/components/app.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import chai, { expect } from 'chai';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import chaiEnzyme from 'chai-enzyme';
 import App from '../../src/components/app';
-
-chai.use(chaiEnzyme());
 
 describe('App' , () => {
 	it('exists', () => {
